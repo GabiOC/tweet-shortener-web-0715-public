45f0b31b610d7b96b55ed838f3f8320e0397851a
GabiOC <oconnor.gabriele@gmail.com>
2015-08-03 18:22:39 -0400

    all done

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,49 @@
 # Write your code here.
+require 'pry'
+
+
+def dictionary
+	dictionary = {
+		"hello" => 'hi',
+		"to" => '2',
+		"two" => '2',
+		"too" => '2',
+		"for" => '4',
+		"four" => '4',
+		'be' => 'b',
+		'you' => 'u',
+		"at" => "@",
+		"and" => "&"
+	}
+end
+
+def word_substituter(tweet)
+	arr = tweet.split(" ")
+
+	arr.map! do |word|
+		if dictionary.has_key?(word.downcase)
+			word = dictionary[word.downcase]
+		else
+			word
+		end
+	end
+	arr.join(" ")
+end
+
+def bulk_tweet_shortener(tweets)
+	tweets.each do |tweet|
+		puts word_substituter(tweet)
+	end
+end
+
+def selective_tweet_shortener(tweet)
+	return word_substituter(tweet) if tweet.length > 140
+	tweet
+end
+
+def shortened_tweet_truncator(tweet)
+	shortened_tweet = nil
+	shortened_tweet = word_substituter(tweet)
+	return shortened_tweet[0...140] if shortened_tweet.length > 140
+	shortened_tweet
+end
